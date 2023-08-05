f2e4835d3d65197efc8cf53ef38757dc91785eb5
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-05 14:00:04 +0530

    Update keywords.py

--- keywords.py
+++ keywords.py
@@ -1,1 +1,11 @@
+Accessing keywords in Python
 
+
+code for the same=
+
+
+                 
+import keyword
+print(keyword.kwlist)
+
+It will print all the 35 keywords present in the Python language 
