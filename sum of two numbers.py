11b6fc9c4055ca802ad170fd6156e90783053e15
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-12 11:21:42 +0530

    Create sum of two numbers.py

---
+++ sum of two numbers.py
@@ -0,0 +1,7 @@
+first=input("enter first number : ")
+second=input("enter second number: ")
+
+sum=int(first)+int(second) 
+print(sum)
+
+[have to use the int before variable in 4th line since they were defined as string earlier which gave the sum of 2 +3 as 23 as they were acted as strings]
