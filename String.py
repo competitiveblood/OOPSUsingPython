4efd4a9e126d7fe48f277283d6c1d6d98029a084
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-12 11:33:51 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,7 @@
+name="Shivangi Singh"
+print(name.find('S'))
+name="Shivangi Singh"
+print(name.upper())
+print(name.lower())
+print(name.find('w'))#prints -1 when that particular letter is not present in the string
+print(name.replace("Shivangi Singh","Nandini Singh"))#replacing strings with another string 
