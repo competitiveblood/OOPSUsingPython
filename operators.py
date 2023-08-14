5d81fb4aaa79d4fc5389de4b8ef4dde0c4da4c1f
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-14 10:50:26 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -3,3 +3,25 @@ print(5-2)#subtraction
 print(5/2)#divison 
 print(5//2)#only the real part,the part after the decimal part will be removed 
 print(5**2)#this is a power operator,5^2=25 will be printed accordingly 
+print(5 * 2)
+print(5 % 2)
+i=i+2
+i += 2
+i -= 2
+i *= 2
+is_greater = 1 > 5
+is_lesser = 1 < 5
+# 1 <= 5
+# 1 >= 5
+is_not_equal = 1 != 5
+is_equal = 1 == 5
+# or -> (atleast one is true)
+# and -> (both are true)
+# not -> (reverses any value)
+number = 2
+print(number > 3)
+print(number < 3)
+print(not number > 3)
+print(not number < 3)
+print(number > 3 and number > 1)
+print(number > 3 or number > 1)
