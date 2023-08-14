7e6067eaf142a6f454a029cfdbfaeb58d4becb7d
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-14 10:37:25 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,1 +1,5 @@
-
+print(5+2)#addition
+print(5-2)#subtraction
+print(5/2)#divison 
+print(5//2)#only the real part,the part after the decimal part will be removed 
+print(5**2)#this is a power operator
