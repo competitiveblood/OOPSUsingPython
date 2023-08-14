998a0fbbb50faf396e63f7e7be61a702bc86a380
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-14 10:38:26 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -2,4 +2,4 @@ print(5+2)#addition
 print(5-2)#subtraction
 print(5/2)#divison 
 print(5//2)#only the real part,the part after the decimal part will be removed 
-print(5**2)#this is a power operator
+print(5**2)#this is a power operator,5^2=25 will be printed accordingly 
