6595c144b8e8bac7a5b610405346bba7aea8d038
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-14 11:54:09 +0530

    Create if-statements.py

---
+++ if-statements.py
@@ -0,0 +1,22 @@
+age = 13
+
+if age >= 18:
+    print("you are an adult")
+    print("you can vote")
+elif age < 3:
+    print("you are a child")
+else:
+    print("you are in school")
+    print("thank you")
+
+
+
+gender = "female"
+
+if gender == "female":
+    print("you are goldman sachs")
+    print("you are employed")
+else:
+    print("you are not goldman sachs")
+    print("no problem")
+    print("better luck next time")
