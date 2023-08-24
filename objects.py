66d714a07984b1b07ca76f1f31c631d1df5d4f75
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-08-24 12:37:57 +0530

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,3 +1,8 @@
+#circle is the class name 
+#__init__ is the constructor name 
+#radius and color are the attributes given 
+#self.radius and self.color is the instance value given to the attributes 
+
 class Circle(object):
 
     def __init__(self, radius=5, color="blue"):
@@ -21,3 +26,9 @@ z = S.add_rad(20)
 # Printing updated attributes
 print(S.radius)
 print(z)
+
+OUTPUT of the code=
+10
+red
+30
+30
