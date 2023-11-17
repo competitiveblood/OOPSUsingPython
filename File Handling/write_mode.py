f5ec092ac028378be760f5e59e3994cb3b0da586
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2023-11-17 21:12:32 +0530

    Create write_mode.py

---
+++ File Handling/write_mode.py
@@ -0,0 +1,4 @@
+# Open a file in write mode
+with open('example.txt', 'w') as file:
+    file.write('This is an example of file handling in Python.\n')
+    file.write('We can write data to a file using the write() method.\n')
